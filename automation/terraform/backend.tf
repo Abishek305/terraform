bb4cf9da2d0b53b313259ce11dd3f612c54b984c
Abishek305 <91280679+Abishek305@users.noreply.github.com>
2025-02-03 16:56:58 +0530

    Create backend.tf

---
+++ automation/terraform/backend.tf
@@ -0,0 +1,18 @@
+terraform {
+#   required_version = ">= 1.7.5, < 1.9.6"
+
+  backend "s3" {
+    encrypt = true
+    bucket = "test-state-terra-projs"
+    region = "us-east-2"
+    key = "terraform/state/terraform.tfstate"
+    # profile = "test-dev"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
