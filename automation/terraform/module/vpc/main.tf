61daa6cc8de77171cd1999ea23624bfab5c5b8de
Abishek305 <91280679+Abishek305@users.noreply.github.com>
2025-03-13 23:11:11 +0530

    Update main.tf

--- automation/terraform/module/vpc/main.tf
+++ automation/terraform/module/vpc/main.tf
@@ -18,30 +18,30 @@ resource "aws_internet_gateway" "igw" {
 
 # private subnet 01
 
-# resource "aws_subnet" "private-subnet-1" {
-#   vpc_id            = aws_vpc.test-vpc.id
-#   cidr_block        = "192.168.10.0/24"
-#   availability_zone = "us-east-1a"
+resource "aws_subnet" "private-subnet-1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_subnet_1_cidr
+  availability_zone       = var.availability_zone_1
 
-#   tags = {
-#     Name                              = "private-subnet"
-#     "kubernetes.io/role/internal-elb" = "1"
-#     "kubernetes.io/cluster/${var.cluster_name}"      = "owned"
-#   }
-# }
-# private subnet 02
+  tags = {
+    Name                              = "${var.environment}-private-subnet-1"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/${var.cluster_name}"      = "owned"
+  }
+}
+#private subnet 02
 
-# resource "aws_subnet" "private-subnet-2" {
-#   vpc_id            = aws_vpc.test-vpc.id
-#   cidr_block        = "192.168.1.0/24"
-#   availability_zone = "us-east-1b"
+resource "aws_subnet" "private-subnet-2" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.private_subnet_2_cidr
+  availability_zone       = var.availability_zone_2
 
-#   tags = {
-#     Name                              = "private-subnet"
-#     "kubernetes.io/role/internal-elb" = "1"
-#     "kubernetes.io/cluster/${var.cluster_name}"      = "owned"
-#   }
-# }
+  tags = {
+    Name                              = "${var.environment}-private-subnet-2"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/${var.cluster_name}"      = "owned"
+  }
+}
 
 # public subnet 01
 
@@ -87,12 +87,52 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table_association" "public-rt" {
+resource "aws_route_table_association" "public-rt1" {
   subnet_id      = aws_subnet.public-subnet-1.id
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table_association" "public-rt1" {
+resource "aws_route_table_association" "public-rt2" {
   subnet_id      = aws_subnet.public-subnet-2.id
   route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_eip" "nat_eip" {
+  depends_on = [aws_internet_gateway.igw]
+  tags = {
+    Name = "nat"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id = aws_subnet.public-subnet-1.id
+
+  tags = {
+    Name = "${var.environment}-nat-gw"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+      cidr_block                 = "0.0.0.0/0"
+      nat_gateway_id             = aws_nat_gateway.nat_gw.id
+    }
+
+  tags = {
+    Name = "${var.environment}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private-rt1" {
+  subnet_id      = aws_subnet.private-subnet-1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private-rt2" {
+  subnet_id      = aws_subnet.private-subnet-2.id
+  route_table_id = aws_route_table.private.id
 }
