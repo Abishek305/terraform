dd9cb147963f7fde0663cf77755df8657ea68c5b
Abishek305 <91280679+Abishek305@users.noreply.github.com>
2025-03-13 23:17:39 +0530

    Update output.tf

--- automation/terraform/output.tf
+++ automation/terraform/output.tf
@@ -1,30 +1,28 @@
 output "vpc_id" {
-  description = "The ID of the created VPC"
   value       = module.vpc.vpc_id
 }
 
-output "eks_cluster_name" {
-  description = "The name of the EKS cluster"
-  value       = module.eks.eks_cluster_name
+output "cluster_name" {
+  value = module.eks.cluster_name
 }
 
-output "eks_cluster_endpoint" {
-  description = "The endpoint of the EKS cluster"
-  value       = module.eks.eks_cluster_endpoint
-}
+# output "eks_cluster_endpoint" {
+#   description = "The endpoint of the EKS cluster"
+#   value       = module.eks.eks_cluster_endpoint
+# }
 
-output "eks_cluster_arn" {
-  description = "The ARN of the EKS cluster"
-  value       = module.eks.eks_cluster_arn
-}
+# output "eks_cluster_arn" {
+#   description = "The ARN of the EKS cluster"
+#   value       = module.eks.eks_cluster_arn
+# }
 
-output "eks_additional_security_group_id" {
-  description = "ID of the additional security group attached to the EKS cluster"
-  value       = module.security-group.security_group_id
-}
+# output "eks_additional_security_group_id" {
+#   description = "ID of the additional security group attached to the EKS cluster"
+#   value       = module.security-group.security_group_id
+# }
 
-output "eks_cluster_id" {
-  description = "The name of the EKS cluster"
-  value       = module.eks.eks_cluster_name
-}
+# output "eks_cluster_id" {
+#   description = "The name of the EKS cluster"
+#   value       = module.eks.eks_cluster_name
+# }
 
