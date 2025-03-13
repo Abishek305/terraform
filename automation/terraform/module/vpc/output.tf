f16445a543a8e17acc865452fb20ce8a4b56aba0
Abishek305 <91280679+Abishek305@users.noreply.github.com>
2025-03-13 23:11:38 +0530

    Update output.tf

--- automation/terraform/module/vpc/output.tf
+++ automation/terraform/module/vpc/output.tf
@@ -19,6 +19,15 @@ output "public_subnet_ids" {
   ]
 }
 
+# Output Private Subnet IDs
+output "private_subnet_ids" {
+  description = "The IDs of the private subnets"
+  value       = [
+    aws_subnet.private-subnet-1.id,
+    aws_subnet.private-subnet-2.id
+  ]
+}
+
 # Output Route Table ID
 output "public_route_table_id" {
   description = "The ID of the public route table"
