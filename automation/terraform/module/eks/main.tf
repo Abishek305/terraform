0717a7d204dd553dfb67853ae0a000e7c100b7be
Abishek305 <91280679+Abishek305@users.noreply.github.com>
2025-03-13 23:09:14 +0530

    Update main.tf

--- automation/terraform/module/eks/main.tf
+++ automation/terraform/module/eks/main.tf
@@ -1,69 +1,111 @@
+# Create managed eks cluster
 
-# create EKS cluster
+resource "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version = var.cluster_version
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
-  cluster_name                   = var.cluster_name
-  cluster_version                = var.cluster_version
-  cluster_endpoint_public_access = true 
-  vpc_id                         = var.vpc_id
-  subnet_ids                     = var.subnet_ids
-  control_plane_subnet_ids       = var.subnet_ids
-  create_cluster_security_group  = true
-  create_node_security_group     = true
-  # cluster_additional_security_group_ids = var.additional_security_group_ids  
-  enable_cluster_creator_admin_permissions = true
-
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-    eks-pod-identity-agent = {
-      most_recent = true
-    }
+  vpc_config {
+    security_group_ids = [
+        aws_security_group.eks_cluster-sg.id,
+        aws_security_group.eks_nodes-sg.id
+    ]
+    subnet_ids = var.subnet_ids
+    endpoint_private_access = true
+    endpoint_public_access = true
   }
 
-  eks_managed_node_group_defaults = {
-    disk_size = var.disk_size
+  tags = {
+    Name = var.cluster_name
+    Environment = var.environment
+    "alpha.eksctl.io/cluster-oidc-enabled" = "true"
   }
-  
 
-  eks_managed_node_groups = {
-    test = {
-      node_name      = var.node_name
-      ami_type       = "AL2_x86_64"
-      min_size       = var.min_size
-      max_size       = var.max_size
-      desired_size   = var.desired_size
-      instance_types = var.instance_types
-      # node_security_group_ids = var.additional_security_group_ids
-      attach_cluster_primary_security_group = false
-      update_config = {
-        max_unavailable = 1
+  access_config {
+    authentication_mode = "API_AND_CONFIG_MAP"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+    aws_security_group.eks_cluster-sg, aws_security_group.eks_nodes-sg
+  ]
+}
+
+# EKS cluster IAM Role
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${var.environment}-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "eks.amazonaws.com"
         }
-      
-      create_launch_template = true
-      launch_template_name   = ""
-      launch_template = {
-        name_prefix             = "custom-launch-template"
-        enable_monitoring       = true
-        instance_market_options = {
-          market_type = "on-demand"
-        }
-      }
-      lifecycle = {
-      create_before_destroy = true
-      }
-    }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+
+# EKS cluster security group 
+resource "aws_security_group" "eks_cluster-sg" {
+  name        = "${var.environment}-cluster-sg"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "${var.environment}-cluster-sg"
+    Environment = "${var.environment}"
   }
 }
 
+resource "aws_security_group_rule" "cluster_inbound" {
+  description              = "Allow worker nodes to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster-sg.id
+  source_security_group_id = aws_security_group.eks_nodes-sg.id
+  to_port                  = 443
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "cluster_outbound" {
+  description              = "Allow cluster API Server to communicate with the worker nodes"
+  from_port                = 1024
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster-sg.id
+  source_security_group_id = aws_security_group.eks_nodes-sg.id
+  to_port                  = 65535
+  type                     = "egress"
+}
+
+resource "aws_eks_addon" "coredns" {
+  cluster_name = aws_eks_cluster.cluster.name
+  addon_name = "coredns"
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name = aws_eks_cluster.cluster.name
+  addon_name = "kube-proxy"
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = aws_eks_cluster.cluster.name
+  addon_name = "vpc-cni"
+}
+
+resource "aws_eks_addon" "pod_identity_agent" {
+  cluster_name = aws_eks_cluster.cluster.name
+  addon_name = "eks-pod-identity-agent"
+}
 
 
